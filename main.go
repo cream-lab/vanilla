3992e79d86988c81a1cd6fd9977f9fb4bdc320d9
taekyeom.oh <taekyeom.oh@nhnent.com>
2016-05-23 23:25:45 +0900

    fixed#1 gulp 적용

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ func main() {
 	router := route.Router()
 
 	n := negroni.Classic()
-  n.Use(negroni.NewStatic(http.Dir("bower_components/jquery/dist")))
-  n.Use(negroni.NewStatic(http.Dir("bower_components/bootstrap/dist")))
 	n.UseHandler(router)
 
 	http.ListenAndServe(":8080", n)
